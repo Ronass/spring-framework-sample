a4a9f76def8c18ed1f39660f00fcda515c0ed59c
matrix <hom_liang@foxmail.com>
2020-09-24 22:07:02 +0800

    #备注#

--- spring-aop/src/main/java/org/matrix/spring/aop/SpringAopApplication.java
+++ spring-aop/src/main/java/org/matrix/spring/aop/SpringAopApplication.java
@@ -2,6 +2,11 @@ package org.matrix.spring.aop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * aop
+ * @author matrix
+ * @datetime 2020/9/24 22:06
+ */
 
 @SpringBootApplication
 public class SpringAopApplication {
