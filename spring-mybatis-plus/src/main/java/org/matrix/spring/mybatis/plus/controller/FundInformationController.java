4a03072c1952ff73c07c51223910cd71dac60572
matrix <517786451@qq.com>
2020-09-27 00:25:39 +0800

    #解决方案#

--- spring-mybatis-plus/src/main/java/org/matrix/spring/mybatis/plus/controller/FundInformationController.java
+++ spring-mybatis-plus/src/main/java/org/matrix/spring/mybatis/plus/controller/FundInformationController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * Q:为什么mybatis-plus中mapper方法不能重载
+ * A:因为mybatis在调用mapper方法的时候是采用动态代理
+ *   即会优先匹配方法执行在第一个的方法，如果返回值，方法名相同，会调用第一个被匹配的方法
+ *   所以会出现异常问题，猜测是与mybatis保存方法的唯一key有关系
+ * 建议:不要重载方法
+ *
  *
  * @author matrix
  * @datetime 2020/9/26 23:42
